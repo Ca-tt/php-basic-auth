408b7baed6db435305355db91859337b4cf9e8d9
Ca-tt <damir96.lukyanenko@gmail.com>
2025-07-16 14:11:47 +0300

    х3

--- index.php
+++ index.php
@@ -51,6 +51,7 @@ $username =  $user['username'] ?? '';
             <!-- пустой бесполезный коммент -->
             <!-- пустой бесполезный коммент -->
             <!-- пустой бесполезный коммент -->
+            <!-- пустой бесполезный коммент -->
 
             <!-- Toggler for mobile -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
