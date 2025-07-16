89546deaad958dd78bed2227e3b872a1a24e59f8
Ca-tt <damir96.lukyanenko@gmail.com>
2025-07-16 14:01:34 +0300

    pre-commit hook is configures

--- index.php
+++ index.php
@@ -48,6 +48,8 @@ $username =  $user['username'] ?? '';
                 Company Co.
             </a>
 
+            <!-- пустой бесполезный коммент -->
+
             <!-- Toggler for mobile -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
